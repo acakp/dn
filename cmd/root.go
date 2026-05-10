c3d639d20e856c3d5f3ed504588c021bcd2c94a9
askar <askar@e.email>
2026-05-10 21:01:23 +0300

    replace nd with dn in help

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "nd",
+	Use:   "dn",
 	Short: "Create daily note",
 	// Long: ``,
 	// Uncomment the following line if your bare application
